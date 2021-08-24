7a6204bf926864cbe060ac8b36e064fcb5ec75e5
ElenaS82 <sergeevaelenavl@gmail.com>
2021-08-24 19:13:55 +0300

    feat: add main script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
+function towelSort (matrix) {
+  if ( arguments.length == 0) {
+    return [];
+   }
+   if ( matrix.length == 0) {
+    return [];
+   }
+    for (let i = 1; i < matrix.length; i++) {
+    matrix[i].reverse();
+    i = i + 1;
+   }
+   let arr = [];
+   arr = (matrix.join(",")).split (",");
+   return arr.map(Number);
+}
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+
   return [];
 }
